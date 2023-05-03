7f59c41f10818e374c945b78e5352ebce4e2a1da
davidvida <classlifedavis@gmail.com>
2023-05-03 13:35:21 -0400

    Add Env variables config for vite (#33)
    Co-authored-by: David Vida <david.vida@fundacion-jala.org>

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 
-export const API_BASE_URL = 'https://todo-service-0qq0.onrender.com/api/v1/';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const Api = createApi({
   reducerPath: 'todoApi',
